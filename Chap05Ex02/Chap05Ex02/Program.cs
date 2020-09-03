24d4379ef70a0d8353079a1a0bfa012a22189ff2
MinerOAO <MinerOAO@outlook.com>
2020-09-04 00:59:59 +0800

    20/09/04

--- Chap05Ex02/Chap05Ex02/Program.cs
+++ Chap05Ex02/Chap05Ex02/Program.cs
@@ -1,20 +1,42 @@
-﻿namespace Chap05Ex02
+﻿using static System.Console;
+using static System.Convert;
+namespace Chap05Ex02
 {
     //枚举enum
-    enum orient_1 : byte//指定基本类型，默认为int,可设为s/byte,u/short,u/int,u/long
+    enum orient : byte//指定基本类型，默认为int,可设为s/byte,u/short,u/int,u/long
     {
         north = 1,//可指定，默认为0
         south = 2,
         east = 3,
         west = 4
     }
+    struct route
+    {
+        public orient direction;
+        public float distance;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            orient_1 dir_orient = orient_1.south;
-            System.Console.WriteLine($"Direction:{dir_orient}");
-            System.Console.ReadKey();
+            route myroute;
+            float distance = -1;
+            int direction = -1;
+            do
+            {
+                WriteLine("Select a direction:");
+                direction = ToInt32(ReadLine());
+            }while (direction < 1 || direction > 4);
+
+            do
+            {
+                WriteLine("Enter distance:");
+                distance = ToSingle(ReadLine());
+            }while (distance < 0);
+            myroute.distance = distance;
+            myroute.direction = (orient)direction;
+            WriteLine($"Direction:{myroute.direction} Distance:{myroute.distance}");
+            ReadKey();
         }
     }
 }
