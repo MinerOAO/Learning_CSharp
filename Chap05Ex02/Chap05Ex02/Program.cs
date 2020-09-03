b2c0dec6e6d6a3cb4ec83964db49c1a65184b866
MinerOAO <MinerOAO@outlook.com>
2020-09-03 22:57:11 +0800

    20/09/03

---
+++ Chap05Ex02/Chap05Ex02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Chap05Ex02
+{
+    //枚举enum
+    enum orient_1 : byte//指定基本类型，默认为int,可设为s/byte,u/short,u/int,u/long
+    {
+        north = 1,//可指定，默认为0
+        south = 2,
+        east = 3,
+        west = 4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            orient_1 dir_orient = orient_1.south;
+            System.Console.WriteLine($"Direction:{dir_orient}");
+            System.Console.ReadKey();
+        }
+    }
+}
