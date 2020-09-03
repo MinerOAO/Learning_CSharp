5a98cfe0ac8d249f7026caaad40331ac01307cb9
MinerOAO <MinerOAO@outlook.com>
2020-09-03 18:16:53 +0800

    Chapter04Ex04,20/09/03

---
+++ Chap04Ex01/Chap04Ex01/Program.cs
@@ -0,0 +1,28 @@
+﻿#region directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+#endregion
+namespace Chap04Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int interger;
+            bool test;
+            WriteLine("Please enter a interger:");
+            interger = ToInt32(ReadLine());
+            test = interger > 10;
+            if (test == false)
+                WriteLine("True!");
+            else
+                WriteLine("False !");
+            ReadKey();
+        }
+    }
+}
