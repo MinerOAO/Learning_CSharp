9a8cd25e9f1a5be3bbda35733ca9ad80ebf3e317
MinerOAO <MinerOAO@outlook.com>
2020-09-03 20:32:49 +0800

    Chapter04Ex03,20/09/03

---
+++ Chap04Ex03/Chap04Ex03/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+
+namespace Chap04Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name_1 = "benjamin";
+            const string name_2 = "andrea";
+            const string name_3 = "ploopy";
+            string input;
+            WriteLine("Please enter your name:");
+            input = ReadLine();
+            switch(input.ToLower())
+            {
+                case name_1:
+                    WriteLine("Same name");
+                    break;
+                case name_2:
+                    WriteLine("Good name");
+                    break;
+                case name_3:
+                    WriteLine("Bad name");
+                    break;
+            }
+            WriteLine($"Hello,{input.ToLower()}!");
+            ReadKey();
+        }
+    }
+}
