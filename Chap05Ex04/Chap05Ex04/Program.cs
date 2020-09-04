751fca93148573a7cdf78d21afdbd6ae615de495
MinerOAO <MinerOAO@outlook.com>
2020-09-04 13:39:48 +0800

    20/09/04,*switch in C#

--- Chap05Ex04/Chap05Ex04/Program.cs
+++ Chap05Ex04/Chap05Ex04/Program.cs
@@ -10,12 +10,19 @@ namespace Chap05Ex04
     {
         static void Main(string[] args)
         {
-            string[] friendnames = { "Andy", "Kevin", "Holton" };
-            int i = 0,j = friendnames.Length;
-            Console.WriteLine($"Here are {j} of my friend{(j > 1 ? "s ":" ")}:");
+            string[] friendnames_1 = { "Andy", "Kevin", "Holton" };
+            string[] friendnames_2 = { "Tsunomaki Watame" };
+            int i = 0,j = friendnames_1.Length;
+            Console.WriteLine($"Here {(j > 1 ? $"are {j} of my friends" : "is 1 of my friend")}");
             for(;i<j;++i)
             {
-                Console.WriteLine(friendnames[i]);
+                Console.WriteLine(friendnames_1[i]);
+            }
+            j = friendnames_2.Length;
+            Console.WriteLine($"Here {(j > 1 ? $"are {j} of my friends" : "is 1 of my friend")}");
+            foreach(string name in friendnames_2)
+            {
+                Console.WriteLine(name);
             }
             Console.ReadKey();
         }
---
+++ Chap05Ex05/Chap05Ex05/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Chap05Ex05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Pattern Matching
+            string[] friendnames = { "Todd Anthony", "Kevin Holton", "Shane Laigle", null, "" };
+            foreach (var name in friendnames)//var
+            {
+                switch (name)
+                {
+                    case string t when t.StartsWith("T"):
+                        WriteLine($"This friend name starts with a 'T',{name}");
+                        break;
+                    case string e when e.Length==0://when语句附加条件
+                        WriteLine("No value");
+                        break;
+                    case null:
+                        WriteLine("NULL value");
+                        break;
+                    case var x://任意其他类型
+                        WriteLine($"This is a {x.GetType().Name}");
+                        break;
+                    default:
+                        break;
+                }
+             
+
+            }
+            #endregion
+            int sum = 0, total = 0, counter = 0, intvalue = 0;
+            int?[] myintarray = new int?[7] { 5, intvalue, 9, 10, null, 2, 99 };//?使int数组可包含空对象
+            foreach (var integer in myintarray)
+            {
+                switch(integer)
+                {
+                    case 0:
+                        WriteLine($"Integer number'{total}' has a value of 0");
+                        ++total;
+                        break;
+                    case int value:
+                        ++counter;++total;sum += value;
+                        WriteLine($"Integer number'{total}' has a value of {value}");
+                        break;
+                    case null:
+                        WriteLine($"Integer number'{total}' is null");
+                        ++total;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            WriteLine($"There are {total} integers.{counter} of them got values.The sum of them is {sum}. ");
+            ReadKey();
+        }
+    }
+}
