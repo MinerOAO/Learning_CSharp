8d3ef65ca5a824235d7e47715c8e43f91b9ccb16
MinerOAO <MinerOAO@outlook.com>
2020-09-04 11:47:38 +0800

    20/09/04

---
+++ Chap05Ex04/Chap05Ex04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap05Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendnames = { "Andy", "Kevin", "Holton" };
+            int i = 0,j = friendnames.Length;
+            Console.WriteLine($"Here are {j} of my friend{(j > 1 ? "s ":" ")}:");
+            for(;i<j;++i)
+            {
+                Console.WriteLine(friendnames[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
