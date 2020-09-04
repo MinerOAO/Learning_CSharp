41c3e6810ea314e99f47a9dbdc8bc571add2ff92
MinerOAO <MinerOAO@outlook.com>
2020-09-04 16:30:59 +0800

    20/09/04，params参数数组

---
+++ Chap06Ex03/Chap06Ex03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Chap06Ex03
+{
+    class Program
+    {
+        static int SumVals(params int[] vals)//params 参数数组
+        {
+            int sum = 0;
+            foreach(int val in vals)
+            {
+                sum += val;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            int sum = SumVals(1, 4, 6, 2, 6, 293, 34, 35);
+            WriteLine($"The summary is {sum}");
+            ReadKey();
+        }
+    }
+}
